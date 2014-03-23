dfe24f2c1c38e396e43a8f9a5d59738a5211e4c7
Tom Brennan <tommybrennan@gmail.com.com>
2014-03-23 19:06:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlynnDance::Application.config.secret_key_base = '8879dc312de8f8189d58db0550bbd7c2baa939c6a58eb3e762c42f158b26c8623b3583aa6bfc3b8855b89e911daff16a59450c5d069ed2121aeada737d8983e5'
