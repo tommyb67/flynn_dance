7214e6033cbc2e6b2e943378fbd492e9a0ad79b9
Tom Brennan <tommybrennan@gmail.com.com>
2014-04-12 12:57:08 -0400

    create the admin module and the namespace admin route for admin lessons

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ FlynnDance::Application.routes.draw do
   get 'about' => 'welcome#about'
 
   resources :lessons
-  resources :admins
+
+  namespace :admin do
+    constraints subdomain: 'admin' do
+      resources :lessons
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
