9d28bb81b89ead0e18ef56c88dfd7229a829d1df
Tom Brennan <tommybrennan@gmail.com.com>
2014-04-12 12:32:13 -0400

    creating build_lessons structure

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ FlynnDance::Application.routes.draw do
   get 'about' => 'welcome#about'
 
   resources :lessons
+  resources :build_lessons
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
